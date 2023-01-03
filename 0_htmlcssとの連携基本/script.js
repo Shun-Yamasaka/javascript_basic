c7839ea0558bdd8dd31abab228ae94a986a76971
shun yamasaka <118syuunico@gmail.com>
2023-01-04 00:28:55 +0900

    html、css、jsとの連携基本

---
+++ 0_htmlcssとの連携基本/script.js
@@ -0,0 +1,24 @@
+/// 通常のアラート
+//alert('別ファイルに記載');
+
+/// 確認ダイアログ
+// const result = confirm('削除してもよいですか？');
+// if(result){
+//     // OKクリック
+//     alert('OK');
+// }else{
+//     // キャンセルクリック
+//     alert('キャンセル');
+// }
+
+/// setTimeout関数（指定した数秒後に実行する）
+// function delayTask(){
+//     alert('1秒経過');
+// }
+// // 1秒後に関数を実行
+// setTimeout(delayTask, 1000);
+// // 1秒毎に繰り返し関数を実行
+// setInterval(delayTask, 1000);
+
+// ウィンドウを開く open(URL, ウィンドウ名の指定, オプション)
+open('samp.html', 'window name', 'width=400, height=200');
